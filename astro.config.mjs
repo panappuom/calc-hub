938ca433350d2ac922eabf7588a30a98985e22ca
omochiforts <omochiforts@gmail.com>
2025-09-03 16:54:50 +0900

    astro.config.mjs に base を設定

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 export default defineConfig({
+  site: 'https://panappuom.github.io/calc-hub',
+  base: '/calc-hub',
   output: 'static',
+  trailingSlash: 'always',   // /calculators/ で出力させる
   srcDir: 'src',
   server: { port: 4321 }
 });
