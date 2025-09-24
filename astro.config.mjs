e9198597738eb4febba165794fc83369cfa24fdc
panappuom <126314094+panappuom@users.noreply.github.com>
2025-09-24 11:54:41 +0900

    feat: support codex base configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+
+const deployTarget = process.env.PUBLIC_DEPLOY_TARGET;
+const isProd = deployTarget === 'prod';
+
 export default defineConfig({
   site: 'https://panappuom.github.io/calc-hub',
-  base: '/calc-hub',
+  base: isProd ? '/calc-hub/' : '/',
   output: 'static',
   trailingSlash: 'always',   // /calculators/ で出力させる
   srcDir: 'src',
